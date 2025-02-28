f7542aea598410ee74cfecf928096cacf26f203b
Dicky Jiang <dickyjuwono@gmail.com>
2025-02-28 12:57:21 +0700

    add GTM to master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/gtm'],
+  gtm: {
+    id: 'G-BNWTEJPQY2', // Your GTM ID
+    enabled: true, // Enable in all environments
+    debug: false, // Enable debug mode
+  }
 })
