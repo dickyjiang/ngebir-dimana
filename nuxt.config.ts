db40b04018dbaa8ac605b1585d6c7efa058ab033
Dicky Jiang <dickyjuwono@gmail.com>
2025-03-07 15:20:53 +0700

    test instal gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/gtm'],
-  gtm: {
-    id: 'G-BNWTEJPQY2', // Your GTM ID
-    enabled: true, // Enable in all environments
-    debug: false, // Enable debug mode
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
 })
