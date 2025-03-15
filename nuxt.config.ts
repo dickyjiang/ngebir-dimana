041fad06dc309c921099c3612fd2a2518fd305a9
Dicky Jiang <dickyjuwono@gmail.com>
2025-03-15 17:32:15 +0700

    add cafe terdekat filter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,22 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
- sourcemap: {
+  sourcemap: {
     server: true,
     client: true
+  },
+  vite: {
+    server: {
+      hmr: {
+        protocol: 'ws',
+        host: 'localhost'
+      }
+    }
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    viewer: true,
   }
 })
