0ba3ff4b04c1e697d362c37d091a6f4bc4a9765a
Dicky Jiang <dickyjuwono@gmail.com>
2025-04-19 14:05:55 +0700

    update supabse anon key

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export default defineNuxtPlugin(nuxtApp => {
   const supabaseUrl = 'https://iblcxviqmqiutjzxnblx.supabase.co'
-  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlibGN4dmlxbXFpdXRqenhuYmx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNDI0MzMsImV4cCI6MjA1NDgxODQzM30._f73-K4lEoe3WmvDQcuXLZPzoPNafhSjQXdsbV4sNbU'
+  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlibGN4dmlxbXFpdXRqenhuYmx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4ODUyMDQsImV4cCI6MjA2MDQ2MTIwNH0.CFQupP7zz_6Skc330_URoP9G_TFqV5B_zibwQ9m-JZg'
   const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
   nuxtApp.provide('supabase', supabase)
