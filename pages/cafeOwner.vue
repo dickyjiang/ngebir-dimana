3a1b5e6592066bce98ff9b7437a65b7effb20720
Dicky Jiang <dickyjuwono@gmail.com>
2025-05-02 14:22:59 +0700

    start cafeOwner page

---
+++ pages/cafeOwner.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Cafe Owner Page</h1>
+        <p>Welcome to the Cafe Owner page!</p>
+        <p>Here you can manage your cafe's information and view customer feedback.</p>
+    </div>
+</template>
