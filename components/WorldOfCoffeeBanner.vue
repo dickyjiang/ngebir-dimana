3f7d3e5ff17b303a22dda262c88e5b133dcbebfa
Dicky Jiang <dickyjuwono@gmail.com>
2025-05-10 15:55:11 +0700

    done styling wcc

--- components/WorldOfCoffeeBanner.vue
+++ components/WorldOfCoffeeBanner.vue
@@ -1,28 +1,35 @@
 <template>
-  <div
-    class="bg-[#FFFDEE] shadow-lg p-2 rounded-lg text-center flex gap-4 items-center justify-between"
+  <a 
+    href="https://asia.worldofcoffee.org/"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="block hover:opacity-90 transition-opacity"
   >
-    <img
-      src="/src/assets/img/world_of_coffee_jakarta_2025.png"
-      alt="world_of_coffee_jakarta_2025"
-      class="h-24 object-cover"
-    />
-    <div>
-      <h3 class="text- font-bold text-[#F15A2D] mb-2">
-        Pameran World of Coffee 2025 Hadir di Jakarta Bersamaan dengan World
-        Brewers Cup
-      </h3>
-      <p class="text-gray-600 text-sm">
-        15-17 Mei 2025 di Jakarta International Convention Center (JICC),
-        Indonesia
-      </p>
+    <div
+      class="bg-[#FFFDEE] border border-[#F15A2D] shadow-lg p-3 rounded-lg text-center flex flex-col sm:flex-row gap-4 items-center justify-between mb-8"
+    >
+      <img
+        src="/src/assets/img/world_of_coffee_jakarta_2025.png"
+        alt="world_of_coffee_jakarta_2025"
+        class="h-16 sm:h-18 object-cover"
+      />
+      <div>
+        <h3 class="text-lg font-bold text-[#F15A2D] mb-3 sm:mb-0 leading-tight">
+          Pameran World of Coffee 2025 Hadir di Jakarta Bersamaan dengan World
+          Brewers Cup
+        </h3>
+        <p class="text-gray-600 text-sm mb-2 sm:mb-0">
+          15-17 Mei 2025 di Jakarta International Convention Center (JICC),
+          Indonesia
+        </p>
+      </div>
+      <img
+        src="/src/assets/img/world_of_coffee_jakarta_2025_wwc-jakarta.png"
+        alt="wcc-jakarta-2025"
+        class="h-10 sm:h-10 object-cover"
+      />
     </div>
-    <img
-      src="/src/assets/img/world_of_coffee_jakarta_2025_wwc-jakarta.png"
-      alt="wcc-jakarta-2025"
-      class="h-12 object-cover"
-    />
-  </div>
+  </a>
 </template>
 
 <script setup lang="ts">
