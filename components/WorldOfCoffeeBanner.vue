3e7cc45790e63943e8f0704e2310a24e675c152c
Dicky Jiang <dickyjuwono@gmail.com>
2025-05-10 16:03:48 +0700

    done styling dan add link

--- components/WorldOfCoffeeBanner.vue
+++ components/WorldOfCoffeeBanner.vue
@@ -11,10 +11,10 @@
       <img
         src="/src/assets/img/world_of_coffee_jakarta_2025.png"
         alt="world_of_coffee_jakarta_2025"
-        class="h-16 sm:h-18 object-cover"
+        class="h-14 lg:h-18 object-cover"
       />
       <div>
-        <h3 class="text-lg font-bold text-[#F15A2D] mb-3 sm:mb-0 leading-tight">
+        <h3 class="text-sm  lg:text-lg font-bold text-[#F15A2D] mb-3 sm:mb-2 leading-tight">
           Pameran World of Coffee 2025 Hadir di Jakarta Bersamaan dengan World
           Brewers Cup
         </h3>
@@ -26,7 +26,7 @@
       <img
         src="/src/assets/img/world_of_coffee_jakarta_2025_wwc-jakarta.png"
         alt="wcc-jakarta-2025"
-        class="h-10 sm:h-10 object-cover"
+        class="h-8 lg:h-10 object-cover"
       />
     </div>
   </a>
