aa4f761c2a5b868b232032fe41a9596bee619654
budi sentosa <budi@avasoft.co>
2025-05-10 15:32:33 +0700

    wcc

---
+++ components/WorldOfCoffeeBanner.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="bg-[#FFFDEE] shadow-lg p-2 rounded-lg text-center flex gap-4 items-center justify-between"
+  >
+    <img
+      src="/src/assets/img/world_of_coffee_jakarta_2025.png"
+      alt="world_of_coffee_jakarta_2025"
+      class="h-24 object-cover"
+    />
+    <div>
+      <h3 class="text- font-bold text-[#F15A2D] mb-2">
+        Pameran World of Coffee 2025 Hadir di Jakarta Bersamaan dengan World
+        Brewers Cup
+      </h3>
+      <p class="text-gray-600 text-sm">
+        15-17 Mei 2025 di Jakarta International Convention Center (JICC),
+        Indonesia
+      </p>
+    </div>
+    <img
+      src="/src/assets/img/world_of_coffee_jakarta_2025_wwc-jakarta.png"
+      alt="wcc-jakarta-2025"
+      class="h-12 object-cover"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  // No props or other setup needed for this simple banner component
+</script>
+
+<style scoped>
+  /* No additional styles needed as they are inline in the template */
+</style>
