800375a90e70ffe9b7ad91f65b8e7e7fdfa6f316
budi sentosa <budi@avasoft.co>
2025-05-19 11:55:44 +0700

    test push

--- layouts/member.vue
+++ layouts/member.vue
@@ -10,18 +10,18 @@
 </template>
 
 <script setup>
-  import Nav from '~/components/NavbarMember.vue';
-  import Footer from '~/components/Footer.vue';
-  import { useSeo } from '~/composables/useSeo';
+import Nav from '~/components/NavbarMember.vue'
+import Footer from '~/components/Footer.vue'
+import { useSeo } from '~/composables/useSeo'
 
-  // Default SEO setup for all pages
-  useSeo({
-    title: 'Website Paling Lengkap buat Cari Tempat Ngopi!',
-    description: 'Satu Klik, Ribuan Cafe! Temukan yang Pas untuk Kamu.',
-    image: '/img/OG-img.png',
-    url: 'https://ngopi.di-mana.com',
-    type: 'website',
-  });
+// Default SEO setup for all pages
+useSeo({
+  title: 'Website Paling Lengkap buat Cari Tempat Ngopi!',
+  description: 'Satu Klik, Ribuan Cafe! Temukan yang Pas untuk Kamu.',
+  image: '/img/OG-img.png',
+  url: 'https://ngopi.di-mana.com',
+  type: 'website',
+})
 </script>
 
 <style scoped></style>
